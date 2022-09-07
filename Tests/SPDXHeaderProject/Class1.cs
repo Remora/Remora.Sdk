0556102c79c2037c64ece4fb1ead8e005abdb64d
Jarl Gullberg <jarl.gullberg@gmail.com>
2022-09-07 18:21:16 +0200

    Add support for SPDX file headers.

---
+++ Tests/SPDXHeaderProject/Class1.cs
@@ -0,0 +1,14 @@
+﻿//
+//  SPDX-FileName: Class1.cs
+//  SPDX-FileCopyrightText: Copyright (c) Jarl Gullberg
+//  SPDX-License-Identifier: LGPL-3.0-or-later
+//
+
+namespace SPDXHeaderProject;
+
+/// <summary>
+/// Some documentation.
+/// </summary>
+public class Class1
+{
+}
